b76ff5cac57fa9cf10f982dd7ac4901fb465b310
Deonte <deonte.bayliss@dcmail.ca>
2022-04-02 23:44:25 -0400

    added live site

---
+++ Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/contacts";
+export const RemoteURI = "mongodb+srv://deonte:<305iqaIlKFuYD0hA>@cluster0.cmjxj.mongodb.net/contacts?retryWrites=true&w=majority"
+export const HostName = "MongoDB Atlas";
+export const SessionSecret = "WEBD6201SessionSecret";
